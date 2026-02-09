1111723e8dd21212aa6375e62238dc8e010d2a5e
ayushi-patil2312 <ayushipatil982@gmail.com>
2026-02-09 17:27:51 +0530

    Create useProducts.js

---
+++ src/hooks/src/hooks/useProducts.js
@@ -0,0 +1,17 @@
+
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../services/api";
+
+export function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchProducts().then(data => {
+      setProducts(data);
+      setLoading(false);
+    });
+  }, []);
+
+  return { products, loading };
+}
